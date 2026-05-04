407cd5af89006b29760a255f3656f50752e26abf
kovalewvladimir <kovalew.vladimir@gmail.com>
2026-05-04 22:10:34 +0300

    Исправить проверку сертификата: добавить проверку обновления сертификата на текущую дату

--- internal/checker/ssl.go
+++ internal/checker/ssl.go
@@ -31,12 +31,28 @@ func CheckSSL(host string, port int, log *slog.Logger) bool {
 	}
 
 	cert := certs[0]
-	now := time.Now()
-	if now.After(cert.NotAfter) {
-		log.Error("ssl: сертификат истёк", "addr", addr, "expired", cert.NotAfter)
+
+	// Сравниваем даты в локальном timezone сервера (настраивается через TZ).
+	// cert.NotBefore приходит в UTC — конвертируем обе стороны в Local,
+	// иначе сертификат выданный в 23:30 UTC (= 02:30 MSK) будет "вчерашним" по UTC.
+	nowLocal := time.Now().In(time.Local)
+	notBeforeLocal := cert.NotBefore.In(time.Local)
+
+	nowY, nowM, nowD := nowLocal.Date()
+	certY, certM, certD := notBeforeLocal.Date()
+
+	if certY != nowY || certM != nowM || certD != nowD {
+		log.Error("ssl: сертификат не обновлён сегодня",
+			"addr", addr,
+			"notBefore", notBeforeLocal.Format("2006-01-02 15:04 MST"),
+			"today", nowLocal.Format("2006-01-02 MST"),
+		)
 		return false
 	}
 
-	log.Info("ssl: ОК", "addr", addr, "expires", cert.NotAfter.Format("2006-01-02"))
+	log.Info("ssl: сертификат обновлён сегодня",
+		"addr", addr,
+		"notBefore", notBeforeLocal.Format("2006-01-02 15:04 MST"),
+	)
 	return true
 }
